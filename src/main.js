b5364c3c31e845c7bb1718dd810b7cb543b68f4b
songyi4669 <songyi4669@gmail.com>
2021-04-24 21:53:14 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,50 @@
+//json파일 패치하기
+function loadItems() {
+    return fetch('data/data.json')
+        .then(response => response.json())
+        .then(json => json.items);
+}
+
+//items 리스트를 업데이트 하기
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//데이터 파일을 HTML 리스트로 만들기
+function createHTMLString(item) {
+    return `
+      <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+        <span class="item_thumbnail">${item.gender}, ${item.size}</span>
+      </li>
+    `;
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+//main
+loadItems()
+    .then(items => {
+     displayItems(items);
+     setEventListeners(items)
+})
+    .catch(console.log);
